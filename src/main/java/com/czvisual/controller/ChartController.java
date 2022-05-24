b5cd9d6f2cefee827a8854a4db2808081fe3051c
socphob <1551391114@qq.com>
2022-05-24 21:17:16 +0800

    完善用户管理相关操作

--- src/main/java/com/czvisual/controller/ChartController.java
+++ src/main/java/com/czvisual/controller/ChartController.java
@@ -6,25 +6,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/chartAnalysis")
 @Controller
 public class ChartController {
-    @RequestMapping("wbgt")
-    public String wbgt(){
-        return "/view/hsmanage/overview.html";
-    }
-
-    @RequestMapping("zzmsd")
-    public String zzmsd(){
-        return "/view/hsmanage/tableview.html";
-    }
-    @RequestMapping("hjwd")
-    public String hjwd(){
-        return "/view/hsmanage/tableview.html";
-    }
-    @RequestMapping("zzmwd")
-    public String zzmwd(){
-        return "/view/hsmanage/tableview.html";
-    }
-    @RequestMapping("zzmlc")
-    public String zzmlc(){
-        return "/view/hsmanage/tableview.html";
+    @RequestMapping("chart")
+    public String table(){
+        return "/view/chart/chart.html";
     }
 }
